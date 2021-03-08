7490b1bf151a5e31eb06c2cf539b3b9920910e7c
tanmay patil <tanmay.admirer@gmail.com>
2021-03-08 20:35:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/textspeech/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.textspeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.b1);
+        editText=findViewById(R.id.e1);
+
+        textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Write something first", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    textToSpeech.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        if (textToSpeech != null)
+        {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+}
